71abf496885f36dc84bf7538f9b65e38d12fdb0f
Shaiful Islam <kuvic16@gmail.com>
2020-04-10 13:31:37 +0600

    specification part one completed

--- specification/src/CustomersRepository.php
+++ specification/src/CustomersRepository.php
@@ -1,8 +1,26 @@
 <?php
 
 class CustomersRepository{
-    public function bySpecification($specification)
+    protected $customers;
+
+    public function __construct(array $customers)
     {
-        return $specification->isSatisfiedBy($customer);
-    }    
+        $this->customers = $customers;
+    }
+    
+    public function matchingSpecification($specification)
+    {
+        $matches = [];
+        foreach($this->customers as $customer){
+            if($specification->isSatisfiedBy($customer)){
+                $matches[] = $customer;
+            }
+        }
+        return $matches;
+    }
+    
+    public function all()
+    {
+        return $this->customers;
+    }
 }
--- specification/tests/CustomersRepositoryTest.php
+++ specification/tests/CustomersRepositoryTest.php
@@ -5,6 +5,28 @@ use PHPUnit\Framework\TestCase;
 // vendor\bin\phpunit --colors tests
 class CustomersRepositoryTest extends TestCase
 {
+    protected $customers;
+
+    protected function setUp(): void
+    {
+        $this->customers = new CustomersRepository(
+            [
+                new Customer('gold'),
+                new Customer('breeze'),
+                new Customer('silver'),
+                new Customer('gold'),
+            ]
+        );
+    }
+
+    /** @test */
+    function test_fetches_all_customers()
+    {
+        $results = $this->customers->all();   
+        $this->assertCount(4, $results);
+    }
+
+    
     function test_fetches_all_customer_who_match_a_given_specification()
     {
         $customers = new CustomersRepository(
@@ -16,7 +38,8 @@ class CustomersRepositoryTest extends TestCase
             ]
         );
         $spec = new CustomerIsGold;
-        $results = $customers->bySpecification(new CustomerIsGold);
+        $results = $customers->matchingSpecification(new CustomerIsGold);
         $this->assertCount(2, $results);
     }
+    
 }
